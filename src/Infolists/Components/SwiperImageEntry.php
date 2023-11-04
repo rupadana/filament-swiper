624e2c68ccf805e100b9c898fb938a37a800cabf
I Wayan Rupadana <rupadanawayan@gmail.com>
2023-11-05 02:44:57 +0800

    adding spatie integration

--- src/Infolists/Components/SwiperImageEntry.php
+++ src/Infolists/Components/SwiperImageEntry.php
@@ -3,15 +3,30 @@
 namespace Rupadana\FilamentSwiper\Infolists\Components;
 
 use Filament\Infolists\Components\ImageEntry;
+use Illuminate\Support\Arr;
 use Rupadana\FilamentSwiper\Infolists\Components\Concerns\HasEffect;
 use Rupadana\FilamentSwiper\Infolists\Components\Concerns\HasPagination;
 use Rupadana\FilamentSwiper\Infolists\Components\Concerns\HasScrollbar;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class SwiperImageEntry extends ImageEntry
 {
     use HasEffect;
     use HasPagination;
     use HasScrollbar;
+
+    protected string | Closure | null $collection = null;
+
+    protected string | Closure | null $conversion = null;
+
+    protected bool $spatie = false;
+
+    public function collection(string | Closure | null $collection): static
+    {
+        $this->collection = $collection;
+
+        return $this;
+    }
 
     const PROGRESSBAR = 'progressbar';
 
@@ -95,5 +110,101 @@ class SwiperImageEntry extends ImageEntry
         $this->centeredSlides = $centeredSlides;
 
         return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getSpatie(): bool
+    {
+        return $this->spatie;
+    }
+
+    /**
+     * @param bool $spatie
+     * @return SwiperImageEntry
+     */
+    public function spatie(bool $spatie = true): SwiperImageEntry
+    {
+        $this->spatie = $spatie;
+        return $this;
+    }
+
+
+
+
+    public function conversion(string | Closure | null $conversion): static
+    {
+        $this->conversion = $conversion;
+
+        return $this;
+    }
+
+    public function getCollection(): ?string
+    {
+        return $this->evaluate($this->collection) ?? 'default';
+    }
+
+    public function getConversion(): ?string
+    {
+        return $this->evaluate($this->conversion);
+    }
+
+    public function getImageUrl(?string $state = null): ?string
+    {
+
+        if($this->getSpatie() == false) {
+            return parent::getImageUrl($state);
+        }
+
+        $record = $this->getRecord();
+
+        if (! $record) {
+            return null;
+        }
+
+        $relationshipName = $this->getRelationshipName();
+
+        if (filled($relationshipName)) {
+            $record = $record->getRelationValue($relationshipName);
+        }
+
+        /** @var ?Media $media */
+        $media = $record->media->first(fn (Media $media): bool => $media->uuid === $state);
+
+        if (! $media) {
+            return null;
+        }
+
+        $conversion = $this->getConversion();
+
+        if ($this->getVisibility() === 'private') {
+            try {
+                return $media->getTemporaryUrl(
+                    now()->addMinutes(5),
+                    $conversion ?? '',
+                );
+            } catch (Throwable $exception) {
+                // This driver does not support creating temporary URLs.
+            }
+        }
+
+        return $media->getAvailableUrl(Arr::wrap($conversion));
+    }
+
+    public function getState(): array
+    {
+
+        if($this->getSpatie() == false) {
+            return parent::getState();
+        }
+
+        $collection = $this->getCollection();
+
+        return $this->getRecord()->getRelationValue('media')
+            ->filter(fn (Media $media): bool => blank($collection) || ($media->getAttributeValue('collection_name') === $collection))
+            ->sortBy('order_column')
+            ->map(fn (Media $media): string => $media->uuid)
+            ->all();
     }
 }
